35c4de98ea66d51668a9414eda8a9f4f34c09d99
armin <fzuvjh>
2022-03-05 23:27:18 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,72 @@
+let a, b, c, d, e, f, g;
+//let ais, cis, dis, eis, fis, gis;
+let ergebnis1 = "a", ergebnis2 = "b", ergebnis3 = "c", ergebnis4 = "d", ergebnis5 = "e", ergebnis6 = "f", ergebnis7 = "g"
+
+function akkordeDzuC() {
+    d = ergebnis3
+    a = ergebnis7
+    g = ergebnis6
+
+
+    if (d){
+        console.log("c")
+    }
+    else if (a) {
+        console.log("g")
+    }
+    else if (g) {
+        console.log("f")
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+/*function akkordePruefen() {
+    a = parseInt(document.getElementById("akkordeeingabe").value);
+    b = parseInt(document.getElementById("akkordeeingabe").value);
+    c = parseInt(document.getElementById("akkordeeingabe").value);
+    d = parseInt(document.getElementById("akkordeeingabe").value);
+    e = parseInt(document.getElementById("akkordeeingabe").value);
+    f = parseInt(document.getElementById("akkordeeingabe").value);
+    g = parseInt(document.getElementById("akkordeeingabe").value);
+}
+
+    if (d, a, g)
+        console.log("noice")
+    else if(d, e, f, g){
+        console.log("auch gut")
+
+    }
+
+
+function akkordeeingabe() {
+    document.getElementById("akkordeeingabe").innerHTML = ergebnis
+
+
+}
+
+function punktePruefen(punkte) {
+
+    if (isNaN(punkte)) {
+
+        console.log(punkte + "keine Zahl!")
+
+    } else  if (punkte >= 0 &&  punkte <= 15) {
+        summe = summe + punkte;
+        anzahlNoten = anzahlNoten + 1;
+    } else {
+        console.log(punkte + "nichts da wallah das geht nicht")
+
+
+    }
+
+}
+*/
