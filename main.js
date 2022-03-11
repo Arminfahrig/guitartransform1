6f118da757a4ff63bc3bbacb735e6070681cecbe
armin <fzuvjh>
2022-03-11 16:27:06 +0100

    heyyy

--- main.js
+++ main.js
@@ -13,10 +13,12 @@ function akkordeDzuC() {
     if (eingabe === d) {
         console.log("c")
         document.getElementById("tf2").value = "c"
-    } else if (eingabe === a) {
-       console.log("g")
+    }
+    else if (eingabe === a) {
+        console.log("g")
         document.getElementById("tf2").value = "g"
-    } else if (eingabe === g) {
+    }
+    else if(eingabe === g) {
         console.log("f")
         document.getElementById("tf2").value = "f"
     }
@@ -28,60 +30,16 @@ function akkordeCzuD() {
     if (eingabe === c) {
         console.log("d")
         document.getElementById("tf2").value = "d"
-
-    } else if (eingabe === g) {
+    }
+    else if (eingabe === g) {
         console.log("a")
         document.getElementById("tf2").value = "a"
-
-    } else if (eingabe === f)
+    }
+    else if (eingabe === f) {
         console.log("g")
         document.getElementById("tf2").value = "g"
-}
-
-
-
-
-/*
-eArrays{
-main (string [],arg){
-string [] array = new string [4];
-array[0] = "a"
-array[1] = "b"
-array[2] = "c"
-array[3] = "d"
-array[4] = "e"
-for (int i = 0; i<array.length.i++){
-system  }
-}
-
-
-/*let ais, cis, dis, eis, fis, gis;
-let ergebnis1 = "a", ergebnis2 = "b", ergebnis3 = "c", ergebnis4 = "d", ergebnis5 = "e", ergebnis6 = "f", ergebnis7 = "g"
-
-
-
-
-
-function akkordeeingabe() {
-    document.getElementById("akkordeeingabe").innerHTML = ergebnis
-
-
-}
-
-function punktePruefen(punkte) {
-
-    if (isNaN(punkte)) {
-
-        console.log(punkte + "keine Zahl!")
-
-    } else  if (punkte >= 0 &&  punkte <= 15) {
-        summe = summe + punkte;
-        anzahlNoten = anzahlNoten + 1;
-    } else {
-        console.log(punkte + "nichts da wallah das geht nicht")
-
-
     }
-
 }
-*/
+
+
+
