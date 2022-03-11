6af819b992affdb83d6aafcf4a9d5d6e08041b56
armin <fzuvjh>
2022-03-11 16:10:36 +0100

    heyy

--- main.js
+++ main.js
@@ -1,22 +1,24 @@
 let a, c, d, f, g;
-let ausgabe, eingabe;
+let  eingabe;
 
 //let ergebnis1 = "a", ergebnis2 = "b", ergebnis3 = "c", ergebnis4 = "d", ergebnis5 = "e", ergebnis6 = "f", ergebnis7 = "g" , b, e
 
-
+/*
 eingabe = document.getElementById("tf1")
 ausgabe = document.getElementById("tf2")
-
+*/
 function akkordeDzuC() {
 
 
-
     if (eingabe === d) {
-        ausgabe = c
+        console.log("c")
+        document.getElementById("tf2").value = "c"
     } else if (eingabe === a) {
-        ausgabe = g
+       console.log("g")
+        document.getElementById("tf2").value = "g"
     } else if (eingabe === g) {
-        ausgabe = f
+        console.log("f")
+        document.getElementById("tf2").value = "f"
     }
 }
 
@@ -25,12 +27,15 @@ function akkordeCzuD() {
 
     if (eingabe === c) {
         console.log("d")
+        document.getElementById("tf2").value = "d"
 
     } else if (eingabe === g) {
         console.log("a")
+        document.getElementById("tf2").value = "a"
 
     } else if (eingabe === f)
         console.log("g")
+        document.getElementById("tf2").value = "g"
 }
 
 
