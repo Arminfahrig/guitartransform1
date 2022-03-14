ae1d2a16a0d2891a01c7b1176088805cf5397c7f
fahrigar <jkbk>
2022-03-14 08:33:45 +0100

    moin

--- main.js
+++ main.js
@@ -1,12 +1,46 @@
-let a, c, d, f, g;
+let a, b, c, d, e, f, g;
 let  eingabe;
 
-//let ergebnis1 = "a", ergebnis2 = "b", ergebnis3 = "c", ergebnis4 = "d", ergebnis5 = "e", ergebnis6 = "f", ergebnis7 = "g" , b, e
+//let ergebnis1 = "a", ergebnis2 = "b", ergebnis3 = "c", ergebnis4 = "d", ergebnis5 = "e", ergebnis6 = "f", ergebnis7 = "g" ,
 
 /*
 eingabe = document.getElementById("tf1")
 ausgabe = document.getElementById("tf2")
 */
+function akkordeDzuA() {
+
+
+    if (eingabe === d) {
+        console.log("a")
+        document.getElementById("tf2").value = "a"
+    }
+    else if (eingabe === a) {
+        console.log("e")
+        document.getElementById("tf2").value = "e"
+    }
+    else if(eingabe === g) {
+        console.log("d")
+        document.getElementById("tf2").value = "d"
+    }
+}
+
+function akkordeDzuB() {
+
+
+    if (eingabe === d) {
+        console.log("b")
+        document.getElementById("tf2").value = "b"
+    }
+    else if (eingabe === a) {
+        console.log("fis")
+        document.getElementById("tf2").value = "fis"
+    }
+    else if(eingabe === g) {
+        console.log("e")
+        document.getElementById("tf2").value = "e"
+    }
+}
+
 function akkordeDzuC() {
 
 
@@ -21,6 +55,91 @@ function akkordeDzuC() {
     else if(eingabe === g) {
         console.log("f")
         document.getElementById("tf2").value = "f"
+    }
+}
+
+function akkordeDzuE() {
+
+
+    if (eingabe === d) {
+        console.log("e")
+        document.getElementById("tf2").value = e = "e"
+    }
+    else if (eingabe === a) {
+        console.log("b")
+        document.getElementById("tf2").value = b = "b"
+    }
+    else if(eingabe === g) {
+        console.log("a")
+        document.getElementById("tf2").value = a = "a"
+    }
+}
+
+function akkordeDzuF() {
+
+
+    if (eingabe === d) {
+        console.log("f")
+        document.getElementById("tf2").value = "f"
+    }
+    else if (eingabe === a) {
+        console.log("c")
+        document.getElementById("tf2").value = "c"
+    }
+    else if(eingabe === g) {
+        console.log("b")
+        document.getElementById("tf2").value = "b"
+    }
+}
+
+function akkordeDzuG() {
+
+
+    if (eingabe === d) {
+        console.log("g")
+        document.getElementById("tf2").value = "g"
+    }
+    else if (eingabe === a) {
+        console.log("d")
+        document.getElementById("tf2").value = "d"
+    }
+    else if(eingabe === g) {
+        console.log("c")
+        document.getElementById("tf2").value = "c"
+    }
+}
+
+function akkordeCzuA() {
+
+
+    if (eingabe === c) {
+        console.log("a")
+        document.getElementById("tf2").value = "a"
+    }
+    else if (eingabe === g) {
+        console.log("e")
+        document.getElementById("tf2").value = "e"
+    }
+    else if (eingabe === f) {
+        console.log("d")
+        document.getElementById("tf2").value = "d"
+    }
+}
+
+function akkordeCzuB() {
+
+
+    if (eingabe === c) {
+        console.log("b")
+        document.getElementById("tf2").value = "b"
+    }
+    else if (eingabe === g) {
+        console.log("fis")
+        document.getElementById("tf2").value = "fis"
+    }
+    else if (eingabe === f) {
+        console.log("e")
+        document.getElementById("tf2").value = "e"
     }
 }
 
@@ -41,5 +160,57 @@ function akkordeCzuD() {
     }
 }
 
+function akkordeCzuE() {
+
+
+    if (eingabe === c) {
+        console.log("e")
+        document.getElementById("tf2").value = "e"
+    }
+    else if (eingabe === g) {
+        console.log("b")
+        document.getElementById("tf2").value = "b"
+    }
+    else if (eingabe === f) {
+        console.log("a")
+        document.getElementById("tf2").value = "a"
+    }
+}
+
+
+function akkordeCzuF() {
+
+
+    if (eingabe === c) {
+        console.log("f")
+        document.getElementById("tf2").value = "f"
+    }
+    else if (eingabe === g) {
+        console.log("c")
+        document.getElementById("tf2").value = "c"
+    }
+    else if (eingabe === f) {
+        console.log("b")
+        document.getElementById("tf2").value = "b"
+    }
+}
+
+
+function akkordeCzuG() {
+
+
+    if (eingabe === c) {
+        console.log("g")
+        document.getElementById("tf2").value = "g"
+    }
+    else if (eingabe === g) {
+        console.log("d")
+        document.getElementById("tf2").value = "d"
+    }
+    else if (eingabe === f) {
+        console.log("c")
+        document.getElementById("tf2").value = "c"
+    }
+}
 
 
